03063fee776597a6782c29c6dcf92298dad732b8
Marcin Gordziejewski <655590+Nicarim@users.noreply.github.com>
2020-12-30 16:03:47 +0100

    Add confirmation dialog

--- internal/list_view.go
+++ internal/list_view.go
@@ -9,6 +9,7 @@ import (
 var app = tview.NewApplication()
 var pages = tview.NewPages()
 var list = tview.NewList()
+var confirmList *tview.List
 var tfStateList []tf.StateOp = nil
 
 func GetListFrame() *tview.Frame {
@@ -19,7 +20,8 @@ func GetListFrame() *tview.Frame {
 	for _, el := range tfStateList {
 		list.AddItem(el.OriginalName, "", 0, nil)
 	}
-	list.AddItem("Quit", "Press to exit", 'q', nil)
+	list.AddItem("Apply", "", 'a', nil)
+	list.AddItem("Cancel", "", 'c', nil)
 
 	frame := tview.NewFrame(list).
 		AddText("Rename State", true, tview.AlignCenter, tcell.ColorWhite)
@@ -40,8 +42,7 @@ func GetRenameView(index int, s *tf.StateOp) *tview.Flex {
 	form := tview.NewForm()
 	form.
 		AddInputField("Rename To", s.OriginalName, 30, nil, func(text string) {
-			s.NewName = text
-			s.Op = tf.RenameOp
+			changedCallback(text, s)
 		}).
 		AddButton("Accept", func() {
 			pages.RemovePage("modal")
@@ -58,6 +59,28 @@ func GetRenameView(index int, s *tf.StateOp) *tview.Flex {
 	return modal
 }
 
+func GetConfirmationList() *tview.Frame {
+	confirmList = tview.NewList()
+	confirmList.ShowSecondaryText(true).
+		SetSelectedFunc(listConfirmHandler)
+
+	for _, el := range tfStateList {
+		if el.Op == tf.NoOp {
+			continue
+		}
+		confirmList.AddItem("[red]"+el.OriginalName+" -> [green]"+el.NewName,
+			"    [white::b]tf state mv "+el.OriginalName+" "+el.NewName,
+			0,
+			nil)
+
+	}
+	confirmList.AddItem("Confirm", "DESTRUCTIVE: runs terraform code visible above", 'c', nil)
+	confirmList.AddItem("Go back", "Correct changes", 'b', nil)
+	frame := tview.NewFrame(confirmList).
+		AddText("Rename plan, confirm actions", true, tview.AlignCenter, tcell.ColorWhite)
+	return frame
+}
+
 func RenderList() {
 	frame := GetListFrame()
 	pages.AddPage("rename", frame, true, true)
@@ -66,16 +89,33 @@ func RenderList() {
 	}
 }
 
+func changedCallback(text string, s *tf.StateOp) {
+	s.NewName = text
+	s.Op = tf.RenameOp
+}
+
 func refreshByIndex(index int) {
 	tfStateAtIndex := tfStateList[index]
 	list.RemoveItem(index)
-	list.InsertItem(index, "[red]"+tfStateAtIndex.OriginalName+" -> [green] "+tfStateAtIndex.NewName, "", 0, nil)
+	list.InsertItem(index, "[red]"+tfStateAtIndex.OriginalName+" -> [green]"+tfStateAtIndex.NewName, "", 0, nil)
 	list.SetCurrentItem(index)
 }
 
+func listConfirmHandler(index int, _ string, _ string, shortcut rune) {
+	if shortcut == 'b' {
+		pages.ShowPage("rename")
+		pages.RemovePage("confirm_rename")
+	}
+}
+
 func listSelectHandler(index int, _ string, _ string, shortcut rune) {
-	if shortcut == 'q' {
+	if shortcut == 'c' {
 		app.Stop()
+		return
+	}
+	if shortcut == 'a' {
+		pages.HidePage("rename")
+		pages.AddPage("confirm_rename", GetConfirmationList(), true, true)
 		return
 	}
 	if tfStateList == nil {
@@ -83,5 +123,4 @@ func listSelectHandler(index int, _ string, _ string, shortcut rune) {
 	}
 	modal := GetRenameView(index, &tfStateList[index])
 	pages.AddPage("modal", modal, true, true)
-	pages.ShowPage("modal")
 }
